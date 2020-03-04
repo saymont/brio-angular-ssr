08609611053fc468b2e2e350e259fd78c590e008
saymonp <saymon.t@outlook.com>
2020-03-04 20:30:49 -0300

    Paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
